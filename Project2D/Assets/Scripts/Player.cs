36441f8ecd58ae6112dfd1a6d4207c62cc8ee816
magicdidac <didacpadilla20@gmail.com>
2019-04-03 01:56:18 +0200

    Cal mejorar el salto

---
+++ Project2D/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    float FallMultiplier { get; }
+    float LowMultiplier { get; }
+    float Speed { get; }
+    public bool CanJump { get; set; }
+    public Rigidbody2D RigidBody { get; set; }
+
+    public Player(float p_Speed, float p_FallMultiplier, Rigidbody2D p_rb)
+    {
+        this.FallMultiplier = p_FallMultiplier;
+        this.Speed = p_Speed;
+        this.RigidBody = p_rb;
+        CanJump = false;
+    }
+
+    public void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            RigidBody.velocity = Vector2.up * FallMultiplier;
+
+            //RigidBody.AddForce(Vector2.up * FallMultiplier, ForceMode2D.Impulse);
+
+            /*else if (_rb.velocity.y > 0 && !Input.GetButtonDown("Jump"))
+            {
+                _rb.velocity += Vector2.up * _lowMultiplier;
+            }*/
+        }
+    }
+}
---
+++ Project2D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Player _player;
+    public float _speed;
+    public float _fallMultiplier;
+    public float _lowMultiplier; //not implemented yet
+
+    void Start()
+    {
+        _player = new Player(_speed, _fallMultiplier, GetComponent<Rigidbody2D>());
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _player.RigidBody.velocity = Vector2.right * _speed;
+        _player.Jump();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider is EdgeCollider2D)
+        {
+            //is the ground
+            _player.CanJump = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider is EdgeCollider2D)
+        {
+            //is the ground
+            _player.CanJump = false;
+        }
+    }
+}
