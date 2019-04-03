601f9a0009c1aba7149db7e479bf70ca39e168b7
magicdidac <didacpadilla20@gmail.com>
2019-04-03 03:17:31 +0200

    First Generation

---
+++ Project2D/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject[] _chunks;
+
+    private int _currentChunk = -1;
+    private int _oldChunk = -1;
+
+    private int _lasPos = 0;
+
+    private void Start()
+    {
+        //changeChunk();
+    }
+
+    private void Update()
+    {
+        if (transform.position.x > _lasPos)
+            changeChunk();
+    }
+
+    private void changeChunk()
+    {
+        int v_rngChunk = -1;
+        do
+        {
+            v_rngChunk = Random.Range(0, _chunks.Length);
+        } while (v_rngChunk == _currentChunk || v_rngChunk == _oldChunk);
+        _oldChunk = _currentChunk;
+        _lasPos += 18;
+        _chunks[v_rngChunk].transform.position = new Vector3(_lasPos, 0, 0);
+        _currentChunk = v_rngChunk;
+    }
+
+}
---
+++ Project2D/Assets/Scripts/TestScripts/CameraMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject _player;
+
+    [SerializeField]
+    private float _offset;
+    
+    void Update()
+    {
+        transform.position = new Vector3(_player.transform.position.x+_offset, 0, -10);
+    }
+}
