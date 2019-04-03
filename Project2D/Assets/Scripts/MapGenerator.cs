64ca9567a9597a761d4fc2902cfb4ef517fb1b66
magicdidac <didacpadilla20@gmail.com>
2019-04-03 18:40:00 +0200

    Fixed generation

--- Project2D/Assets/Scripts/MapGenerator.cs
+++ Project2D/Assets/Scripts/MapGenerator.cs
@@ -15,13 +15,11 @@ public class MapGenerator : MonoBehaviour
     private int _currentChunk = -1;
     private int _oldChunk = -1;
 
-    private float _lasPos = 3.5f;
-
-    private
+    private float _lastPos = 2f;
 
     private void Update()
     {
-        if (transform.position.x > _lasPos)
+        if (transform.position.x > _lastPos)
             changeChunk();
 
         checkChunkOutOfBounds();
@@ -34,8 +32,14 @@ public class MapGenerator : MonoBehaviour
             _nextChunk = Random.Range(0, _chunks.Length);
         } while (_nextChunk == _currentChunk || _nextChunk == _oldChunk);
         _oldChunk = _currentChunk;
-        _lasPos += _chunks[_nextChunk].lenght;
-        Instantiate(_chunks[_nextChunk].prefab, new Vector3(_lasPos, 0, 0), Quaternion.identity, _chunkStorage.transform);
+
+        if(_currentChunk != -1)
+        	_lastPos += _chunks[_currentChunk].lenght/2;
+    	else
+        	_lastPos += _chunks[_nextChunk].lenght/2;
+
+        _lastPos += _chunks[_nextChunk].lenght/2;
+        Instantiate(_chunks[_nextChunk].prefab, new Vector3(_lastPos, 0, 0), Quaternion.identity, _chunkStorage.transform);
         _currentChunk = _nextChunk;
     }
 
