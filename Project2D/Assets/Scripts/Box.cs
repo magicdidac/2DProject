edc3f5760006d70103a9d3f662a9059cf651ceab
magicdidac <didacpadilla20@gmail.com>
2019-06-04 12:08:35 +0200

    Box implementadas y muros implementados Eliminación de Sprites que no se usaban Eliminación de Chunks sin usar

---
+++ Project2D/Assets/Scripts/Box.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+
+    [SerializeField] private Sprite[] sprites;
+
+
+    private void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];
+    }
+
+}
