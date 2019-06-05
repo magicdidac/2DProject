42662598468d6e7a45b414fd519511f0366d7889
magicdidac <didacpadilla20@gmail.com>
2019-06-05 12:53:57 +0200

    Generador de monedas creado Fuel y monedas con el nuevo diseño

---
+++ Project2D/Assets/Scripts/Editor/CoinStructureEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CoinStructure))]
+public class CoinEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+
+        DrawDefaultInspector();
+
+        CoinStructure myStructure = (CoinStructure)target;
+
+        EditorGUILayout.LabelField("Coins Count: ", myStructure.GetCoinsCounter()+"");
+
+        if (GUILayout.Button("Generate"))
+        {
+            myStructure.GenerateStructure();
+        }
+
+
+
+    }
+}
