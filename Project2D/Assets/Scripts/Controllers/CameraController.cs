46f7d957ea08eedd31c84e9d67c2c048f1a80245
magicdidac <didacpadilla20@gmail.com>
2019-06-06 04:37:54 +0200

    Camara arreglada Añadido nuevos Instakill con los efectos Añadida sprite de la bala

--- Project2D/Assets/Scripts/Controllers/CameraController.cs
+++ Project2D/Assets/Scripts/Controllers/CameraController.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController: AController
+public class CameraController : AController
 {
-    
+
 
     [HideInInspector] private float _offset = 4;
 
@@ -19,33 +19,38 @@ public class CameraController: AController
         if (gc.player.transform.position.x < transform.position.x - _offset - .1f)
             return;
 
-        if(gc.GetFloor() != myFloor && gc.player.isGrounded && !gc.player.isTrampoline)
+        if (gc.GetFloor() != myFloor && gc.player.isGrounded && !gc.player.isTrampoline)
             myFloor = gc.GetFloor();
         else if (gc.GetFloor() != myFloor)
         {
-            if(gc.GetFloor() - myFloor > 0)//Subo
+            if (gc.GetFloor() - myFloor > 0)//Subo
             {
-                if(gc.player.transform.position.y < myFloor*8)
+                if (gc.player.transform.position.y < myFloor * 8)
                     transform.position = Vector3.Lerp(transform.position, new Vector3(gc.player.transform.position.x + _offset, (8 * myFloor) + 2, transform.position.z), .5f);
                 else
                     transform.position = Vector3.Lerp(transform.position, new Vector3(gc.player.transform.position.x + _offset, (gc.player.transform.position.y) + 2, transform.position.z), .5f);
             }
             else //Bajo
             {
-                if (gc.player.transform.position.y > myFloor*8)
+                if (gc.player.transform.position.y > myFloor * 8)
                     transform.position = Vector3.Lerp(transform.position, new Vector3(gc.player.transform.position.x + _offset, (8 * myFloor) + 2, transform.position.z), .5f);
                 else
                     transform.position = Vector3.Lerp(transform.position, new Vector3(gc.player.transform.position.x + _offset, (gc.player.transform.position.y) + 2, transform.position.z), .5f);
             }
         }
-            
-        else
-            transform.position = Vector3.Lerp(transform.position, new Vector3(gc.player.transform.position.x + _offset, (8 * gc.GetFloor())+2, transform.position.z), .5f);
-    }
 
-    private void Update()
-    {
-        //indicator.SetActive(myFloor != 0);
+        else
+        {
+            if (gc.player.transform.position.y < myFloor * 8 + 3)
+            {
+                transform.position = Vector3.Lerp(transform.position, new Vector3(gc.player.transform.position.x + _offset, (8 * gc.GetFloor()) + 2, transform.position.z), .5f);
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(transform.position, new Vector3(gc.player.transform.position.x + _offset, gc.player.transform.position.y - 1, transform.position.z), .5f);
+
+            }
+        }
     }
 
 }
