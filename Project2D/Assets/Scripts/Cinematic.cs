5e995d18836887f954e70d7a5c55dd1ad92450a6
magicdidac <didacpadilla20@gmail.com>
2019-06-11 01:44:16 +0200

    Añadida cinematica inicial (con sistema para saltarla)

---
+++ Project2D/Assets/Scripts/Cinematic.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class Cinematic : MonoBehaviour
+{
+
+    [SerializeField] private VideoPlayer video = null;
+    [SerializeField] private int changeSceneIndex = 0;
+
+
+    [HideInInspector] private bool isInvokeActive = false;
+
+    private void Update()
+    {
+        if(!isInvokeActive && video.isPlaying)
+        {
+            isInvokeActive = true;
+            Invoke("changeScene", 23);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            changeScene();
+
+    }
+
+    private void changeScene()
+    {
+        SceneManager.LoadSceneAsync(changeSceneIndex);
+    }
+
+}
